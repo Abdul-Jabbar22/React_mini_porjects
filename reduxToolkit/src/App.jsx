861495525dab3bcd2ca65c83880bcdbbb8426f5b
Abdul Jabbar <123597574+Abdul-Jabbar22@users.noreply.github.com>
2024-07-10 11:57:27 +0500

    add new project

---
+++ reduxToolkit/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+import "./App.css";
+import AddToDo from "./components/AddToDo";
+import Todos from "./components/Todos";
+
+function App() {
+  return (
+    <>
+      <h1>Learn about redux toolkit</h1>
+      <AddToDo />
+      <Todos />
+    </>
+  );
+}
+
+export default App;
